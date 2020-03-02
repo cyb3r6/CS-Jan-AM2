a71934392394c45df85c9ac187ae84cb2151bde4
cyb3r6 <45832126+cyb3r6@users.noreply.github.com>
2020-03-02 15:19:50 -0700

    Session 10
    Created a VRInput script to collect input from our controllers.
    Using custom Input Axis to bind the buttons on our controllers to public floats.
    Using a VRAnimator script to take the values from the VRInput script.
    Using a float value to controller the frames played in "Fist Closing".

---
+++ Jan-AM2/Assets/Scripts/VRAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRAnimator : MonoBehaviour
+{
+    private Animator vrHandAnimator;
+    private VRinput controller;
+
+   
+    void Awake()
+    {
+        vrHandAnimator = GetComponentInChildren<Animator>();
+        controller = GetComponent<VRinput>();
+    }
+
+    
+    void Update()
+    {
+        if (controller)
+        {
+            if (vrHandAnimator)
+            {
+                vrHandAnimator.Play("Fist Closing", 0, controller.gripValue);
+            }
+        }
+    }
+}
---
+++ Jan-AM2/Assets/Scripts/VRinput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRinput : MonoBehaviour
+{
+    public bool isLeftHand;         // if isLeftHand is true, then the script is attached to the left hand
+    public float gripValue;
+    public float triggerValue;
+
+    private string gripAxis;        // store the grip values of our controller
+
+    void Awake()
+    {
+        if (isLeftHand)              // short for isLeftHand == true
+        {
+            gripAxis = "LeftGrip";
+        }
+        else
+        {
+            gripAxis = "RightGrip";
+        }
+
+    }
+
+    
+    void Update()
+    {
+        gripValue = Input.GetAxis(gripAxis);
+        Debug.Log("The grip  value is: " + gripValue);
+    }
+}
